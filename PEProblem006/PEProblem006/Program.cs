a5bd33d587b7d7f56916cd571dd195882bff62e7
Kubik2000 <jakub.drs@gmail.com>
2015-09-22 22:08:25 +0200

    Upgrade to VS2015 .net 4.6
    Recompiled in Microsoft Visual Studio 2015 Community  with .net
    framework 4.6

---
+++ PEProblem006/PEProblem006/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*
+ * The sum of the squares of the first ten natural numbers is,
+ * 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * 
+ * Just simple one cycle run.
+ */
+namespace PEProblem006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime StartTime = DateTime.Now;
+
+            int sumSquare = 0;
+            int squareSum = 0;
+            for (int i = 1; i < 101; i++)
+            {
+                sumSquare += (i * i);
+                squareSum += i;
+            }
+            squareSum = squareSum * squareSum;
+            int result = squareSum - sumSquare;
+
+            TimeSpan elapsedTime = DateTime.Now - StartTime;
+
+            Console.WriteLine("result: " + result.ToString());
+            Console.WriteLine("elapsed time: " + elapsedTime.ToString("mm':'ss':'fff") + " mm:ss:fff");
+            Console.ReadLine();
+        }
+    }
+}
